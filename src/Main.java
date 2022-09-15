b86e3ef4336fb11578ddfcbedc6c3ca0b26ea73e
antonyare93 <antonyare_93@hotmail.com>
2022-09-15 14:15:12 -0500

    ejercicio_tema 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Ejercicio OpenBootcamp
+        int numeroWhile = -5;
+        while (numeroWhile < 3){
+            if (numeroWhile < 0){
+                System.out.println(Integer.toString(numeroWhile) + " es negativo");
+            } else if (numeroWhile > 0){
+                System.out.println(numeroWhile + " es positivo");
+            } else {
+                System.out.println(numeroWhile + " es cero");
+            }
+            numeroWhile++;
+        }
+        do {
+            System.out.println("Do while: " + numeroWhile);
+        } while (numeroWhile < 3);
+        for (int i=0;i<=3;i++){
+            System.out.println("for var: " + i);
+        }
+        int estacion = (int)(Math.random()*4);
+        switch (estacion){
+            case 0:
+                System.out.println("Es primavera");
+                break;
+            case 1:
+                System.out.println("Es verano");
+                break;
+            case 2:
+                System.out.println("Es otoño");
+                break;
+            case 3:
+                System.out.println("Es invierno");
+        }
     }
 }
